51a34b1241f8864f919d23137522f4ef067abf75
Jason Maslanka <jmaslank@uwo.ca>
2020-10-04 18:17:02 -0400

    Added new list with searched list items

--- pokemonSearch.js
+++ pokemonSearch.js
@@ -1,4 +1,11 @@
+function divCreate(){
+    let createDiv = document.getElementById("createDiv");
+    let secondDiv = document.createElement("div");
+    secondDiv.id = "newDiv";
+    createDiv.appendChild(secondDiv);
 
+
+}
 function get_pokemon_name() {
     let form_check = /^[A-Za-z]+$/
     let input_length = document.getElementById('poke_name').value.length;
@@ -19,14 +26,16 @@ function poke_name_search(poke_name) {
     let ul = document.getElementById("pokemon");
     let li = ul.getElementsByTagName("li");
     let user_input = poke_name.toLowerCase();
+    let newDiv = document.getElementById('newDiv');
 
     for (let i = 0; li.length; i++) {
         let a = li[i].getElementsByClassName("name")[0];
         let textValue = a.textContent || a.innerText;
         if (textValue.toLowerCase().indexOf(user_input) > -1) {
-            let listItem = document.createElement("li");
+            let listItem = document.createElement("ul");
             listItem = li[i].cloneNode(true);
-            document.getElementById("newDiv").appendChild(listItem);
+            listItem.id = "searchList";
+;            newDiv.appendChild(listItem);
         } else {
             //li[i].style.display = "none"
         }
