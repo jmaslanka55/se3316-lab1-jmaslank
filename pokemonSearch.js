eafb20f371406ecdb0208afb9f8d7c7e4cfc31d6
Jason Maslanka <jmaslank@uwo.ca>
2020-10-04 16:36:15 -0400

    Fixed Filter to only display current pokemon searched

--- pokemonSearch.js
+++ pokemonSearch.js
@@ -26,8 +26,7 @@ function get_pokemon_name() {
     let form_check = /^[A-Za-z]+$/
     let input_length = document.getElementById('poke_name').value.length;
     let poke_name = document.getElementById('poke_name').value;
-    if (form_check.test(poke_name) && input_length <=20)
-    {
+    if (form_check.test(poke_name) && input_length <= 20) {
         poke_name_search(poke_name);
     }
 }
@@ -40,20 +39,29 @@ function get_pokemon_num() {
 }
 
 function poke_name_search(poke_name) {
-    document.createElement("div");
-    let searched_pokemon = [];
+    let ul = document.getElementById("pokemon");
+    let li = ul.getElementsByTagName("li");
     let user_input = poke_name.toLowerCase();
-    for (let i = 0; i < pokeArray.length; i++) {
-        if (pokeArray[i][1].toLowerCase().includes(user_input)) {
-            searched_pokemon.push([pokeArray[i][0],pokeArray[i][1],pokeArray[i][2]]);
+    let newDiv = document.createElement('div');
+    for (let i = 0; li.length; i++) {
+        let a = li[i].getElementsByClassName("name")[0];
+        let textValue = a.textContent || a.innerText;
+        if (textValue.toLowerCase().indexOf(user_input) > -1) {
+            let listItem = document.createElement('li');
+            listItem = li[i].cloneNode(true);
+            newDiv.appendChild(listItem);
+        } else {
+            li[i].style.display = "none"
         }
-    }
-    for (let j = 0; j<searched_pokemon.length;i++){
-        let listElement = document.getElementById();
-        let textNode = document.createTextNode(searched_pokemon[j][1]);
-        listElement[j].appendChild(textNode);
     }
 }
 
-
+//function poke_num_search(poke_num) {
+//let user_input = poke_num.toLowerCase();
+// for (let i = 0; i < pokeArray.length; i++) {
+// if (parseInt(pokeArray[i][0]) == user_input) {
+//  alert_message += "# " + pokeArray[i][0] + " " + pokeArray[i][1] + " " + pokeArray[i][2] + "\n";
+// }
+// }
+//}
 
